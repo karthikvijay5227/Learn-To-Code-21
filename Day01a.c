3d41020a49129b4f46e9a47250c7b34fb4c1463a
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-05-11 16:41:58 +0530

    Create Day01a.c

---
+++ Day01a.c
@@ -0,0 +1,13 @@
+//Write a C Program to print "Hello World!" and "Welcome to Learn To Code!" in two separate lines
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Hello World!");
+    printf("\nWelcome to Learn To Code!");
+    return 0;
+}
