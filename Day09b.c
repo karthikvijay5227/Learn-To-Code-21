d98c97fb81eba840ef2d35c7eabf654743feaecd
karthikvijay5227 <77284874+karthikvijay5227@users.noreply.github.com>
2021-05-20 09:47:57 +0530

    Create Day09b.c

---
+++ Day09b.c
@@ -0,0 +1,32 @@
+/*Write a C program to print the following number pattern
+    55555
+     4444
+      333
+       22
+        1*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i, j, N;
+    scanf("%d", &N);
+
+    for(i=N; i>=1; i--)
+    {
+        for(j=N; j>i; j--)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=i; j++)
+        {
+            printf("%d", i);
+        }
+
+        printf("\n");
+    }
+  return 0;
+}
